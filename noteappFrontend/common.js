8670b5e3eb63f46c301a7eb2c7d8d55f975aed8d
amarjit <amarjityadav70173@gmail.com>
2024-10-16 08:00:59 +0530

    added login and signup skeleton (html with tailwind css)

---
+++ noteappFrontend/common.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Roboto', 'sans-serif'],
+            },
+            colors: {
+                'primary': '#3B82F6',
+                'secondary': '#10B981',
+                'background': '#F3F4F6',
+                'text': '#1F2937',
+            },
+        }
+    }
+}
